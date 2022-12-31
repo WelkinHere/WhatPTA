ee990c9ab44ed89692da8e63a1cd34ecb996e6c8
WelkinHere <64088501+WelkinHere@users.noreply.github.com>
2022-12-31 20:51:11 +0800

    2-10 update

--- 浙大版《C语言程序设计（第4版）》题目集/编程题/练习2-x/2-10 Solve.c
+++ 浙大版《C语言程序设计（第4版）》题目集/编程题/练习2-x/2-10 Solve.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 int main() {
-    int x;
-    scanf("%d", &x);
+    double x;
+    scanf("%lf", &x);
 
     // 处理部分
+    double ans;    // 存放答案的地方
     if(x != 0) {
-        x = 1 / x;
+        ans = 1 / x;
     }
 
     // 输出部分
-    printf("%d", x);
+    printf("f(%.1lf) = %.1lf\n", x, ans);
 }
+
+/*
+ *  简单的条件判断。
+ */
