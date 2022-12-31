e91253f0f76262fd8cb55fdeccff44fea30ddfad
WelkinHere <64088501+WelkinHere@users.noreply.github.com>
2022-12-31 21:38:30 +0800

    更新了自己的个人习题集解答（没整理过的东西，以后会整理）

---
+++ 浙大版《C语言程序设计（第4版）》题目集/编程题/练习2-x/2-12 solve.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    double lower;      // 输入的小值
+    double upper;      // 输入的大值
+    scanf("%lf%lf", &lower, &upper);
+
+    // 变量部分
+    double ansCelsius; // 输出的摄氏温度
+
+    // 判断是否可以正常输出
+    if(lower > upper) {
+        // 不能正常输出时直接输出 "Invalid."
+        printf("Invalid.");
+    } else {
+        // 输出部分
+        printf("fahr celsius\n");
+
+        // 处理并输出剩余的部分
+        for(;lower <= upper;lower += 2) {
+            ansCelsius = 5 * ((lower - 32) / 9);
+
+            printf("%-5.0lf%6.1lf\n", lower, ansCelsius);
+        }
+    }
+}
