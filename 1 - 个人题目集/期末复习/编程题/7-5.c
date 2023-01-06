4f777938f68f8057f25eddf27da0a4d92db1ef2e
WelkinHere <64088501+WelkinHere@users.noreply.github.com>
2023-01-06 08:44:33 +0800

    7-5 wip

---
+++ 1 - 个人题目集/期末复习/编程题/7-5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// 自定函数，用来输出题目要求星号
+void pStar() {
+    printf("* ");
+}
+
+// 自定函数，用来输出空格占位
+void pEmpty() {
+    printf("  ");
+}
+
+// 自定函数，C++ 标准输出的结尾（我不知道我为什么要怎么做但还是加进去了）
+void endL() {
+    printf("\n");
+}
+
+int main() {
+    int line;   // 行数
+    scanf("%d", &line);
+    
+    // 外层 for 循环控制循环次数
+    
+}
